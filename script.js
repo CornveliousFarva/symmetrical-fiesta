895344a392efcd38687c39d45c25d8bc07cadb90
Greg Schuman <gcschuman@hotmail.com>
2023-03-01 12:27:00 -0800

    Added initial ID calls & the function to get a new quote.

---
+++ script.js
@@ -0,0 +1,11 @@
+// ID calls
+const text = document.getElementById("quote");
+const author = document.getElementById("author");
+const tweetButton = document.getElementById("tweet")
+
+// Function to get a new quote
+const getNewQuote = async () => {
+    // API for quotes
+
+    // Fetch the data from the API
+}
