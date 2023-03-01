a7d2ad72567a2c548d356428e339371e03aecfb9
Greg Schuman <gcschuman@hotmail.com>
2023-03-01 13:05:50 -0800

    Added the api, the data fetch in the API, converting the response to JSON & storing it, and generating a number in the quotes array, which will translate to a quote.

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ const tweetButton = document.getElementById("tweet")
 // Function to get a new quote
 const getNewQuote = async () => {
     // API for quotes
+    var url="https://type.fit/api/quotes";
 
     // Fetch the data from the API
+    const response = await fetch(url);
+    console.log(typeof response);
+
+    // Convert response to JSON and store it in quotes array
+    const allQuotes = await response.json();
+
+    // Generates a random number between 0 and the length of the quotes array
+    const index = Math.floor(Math.random() * allQuotes.length);
+
+    
 }
+
+getNewQuote();
